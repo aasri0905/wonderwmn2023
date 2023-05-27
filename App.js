593a16b6b0228afc472e603b85397a002d30f72b
Aasritha Gopalam <87528934+aasri0905@users.noreply.github.com>
2023-05-27 18:02:35 +0530

    Add files via upload
    init-commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import GeoLocationexpo from "./GeoLocationexpo";
+import { View, Text } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <GeoLocationexpo />
+    </View>
+  );
+};
+
+export default App;
